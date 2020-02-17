6db60b3a379f4cf263b786a898064886f6e9ff11
Cherif Jazra <cherifjazra@Cherifs-MacBook-Pro.local>
2020-02-16 22:45:11 -0800

    update producer stations and turnstile

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -9,6 +9,13 @@ from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
 
+from requests import Session
+import socket
+print (socket)
+hostname = 'localhost'
+# print(socket.gethostname())
+print(socket.getaddrinfo(hostname, 8081))
+
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
@@ -38,10 +45,24 @@ class Producer:
         #
         #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
-        }
+            "bootstrap.servers": "localhost:9092",
+            "schema.registry.url": "http://localhost:8081"
+            }
+
+        self.client = AdminClient(
+                    {
+                      # 'debug': 'admin',                       
+                     "bootstrap.servers": "localhost:9092"
+                     }
+                )
+
+        # s = Session()
+        # s.request("POST", "http://localhost:8081/subjects/com-stations-value/versions",
+        #     {'Accept': 'application/vnd.schemaregistry.v1+json, application/vnd.schemaregistry+json, application/json', 'Content-Length': '1', 'Content-Type': 'application/vnd.schemaregistry.v1+json'},
+        #     {'schema': '{"type": "record", "name": "arrival_value", "namespace": "com_udacity", "fields": [{"type": "long", "name": "station_id"}, {"type": "long", "name": "train_id"}, {"type": "string", "name": "direction"}, {"type": "string", "name": "line"}, {"type": "string", "name": "train_status"}, {"type": ["null", "long"], "name": "prev_station_id"}, {"type": ["null", "string"], "name": "prev_direction"}]}'}
+        #     )
+
+        # exit()
 
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
@@ -49,8 +70,11 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+                    config=self.broker_properties,
+                    default_key_schema=self.key_schema, 
+                    default_value_schema=self.value_schema                    
+                )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
@@ -60,7 +84,19 @@ class Producer:
         # the Kafka Broker.
         #
         #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        logger.info(f"creating topic {self.topic_name}")
+
+        fs = self.client.create_topics(
+                        [NewTopic(self.topic_name, 
+                                  num_partitions=self.num_partitions,
+                                  replication_factor=self.num_replicas)])
+
+        for topic, f in fs.items():
+            try:
+                f.result()  # The result itself is None
+                print("Topic {} created".format(topic))
+            except Exception as e:
+                print("Failed to create topic {}: {}".format(topic, e))
 
     def time_millis(self):
         return int(round(time.time() * 1000))
@@ -72,7 +108,8 @@ class Producer:
         # TODO: Write cleanup code for the Producer here
         #
         #
-        logger.info("producer close incomplete - skipping")
+        logger.info("producer close")
+        self.producer.close()
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
--- producers/models/station.py
+++ producers/models/station.py
@@ -16,10 +16,7 @@ class Station(Producer):
 
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
 
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
         self.name = name
@@ -37,13 +34,13 @@ class Station(Producer):
         # replicas
         #
         #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        topic_name = "com-stations"
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Station.value_schema, 
+            num_partitions=1,
+            num_replicas=1,
         )
 
         self.station_id = int(station_id)
@@ -62,18 +59,28 @@ class Station(Producer):
         # TODO: Complete this function by producing an arrival message to Kafka
         #
         #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        logger.info(self)
+        
+        value_dict = {                
+                    "station_id": int(self.station_id),
+                    "train_id": train.train_id,
+                    "direction": direction,
+                    "line": str(self.color),
+                    "train_status": str(train.status),
+                    "prev_station_id": None,
+                    "prev_direction": None
+                }
+    
+        if prev_direction:
+            value_dict["prev_direction"] = str(prev_direction)
+        if  prev_station_id:
+            value_dict["prev_station_id"] = int(prev_station_id)
+
+        self.producer.produce(
+           topic=self.topic_name,
+           key={"timestamp": self.time_millis()},
+           value=value_dict
+        )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
@@ -102,3 +109,4 @@ class Station(Producer):
         """Prepares the producer for exit by cleaning up the producer"""
         self.turnstile.close()
         super(Station, self).close()
+
